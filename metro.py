39d252b10d6adacba548c6fad8c9e63f16013234
kuba <kuba@kucharski.it>
2013-05-07 23:58:28 +0200

    function "send"

--- metro.py
+++ metro.py
@@ -6,7 +6,7 @@ packet_data = '13ff091b'
 #odczyt statusow
 
 import serial
-ser = serial.Serial('/dev/ttyUSB1',
+serObj = serial.Serial('/dev/ttyUSB1',
                        baudrate=4800,
                        bytesize=serial.EIGHTBITS,
                        parity=serial.PARITY_NONE,
@@ -19,46 +19,33 @@ ser = serial.Serial('/dev/ttyUSB1',
 
 from hexbyte import *
 
-def readbytes(number):
+def readbytes(number,serObj):
     buf = ''
     for i in range(number):
-        byte = ser.read()
+        byte = serObj.read()
         buf += byte
 
     return buf
 
 # binarnego anda z maska 1111111, FFFFFFF
 
-init1 = '10 09 09 22'
-ser.write(HexToByte(init1))
+hexstr = '10 09 09 22'
 
-# listen for the response string: FF 55 04 09 00 00 07 EC
-#print readbytes(4)
-response1 = ByteToHex(readbytes(4))
-#assert response1 == 'FF 55 04 09 00 00 07 EC'
-print response1
 
-# send the second init string
-#init2 = 'FF 55 02 09 05 F0'
-#ser.write(HexToByte(init2))
+def send(hexstr, serObj):
+    serObj.write(HexToByte(hexstr))
+    byte = serObj.read()
+    if byte:
+        message = byte + readbytes(4,serObj)
 
-# listen for the response string: FF 55 04 09 06 00 25 C8
-#response2 = ByteToHex(readbytes(8))
-#assert response2 == 'FF 55 04 09 06 00 25 C8'
-print "and"
-
-byte = ser.read()
-    # if a byte is coming down the port,
-if byte:
-        # get the rest of the message (34 chars)
-    message = byte + readbytes(4)
-        # and decipher it
-    print message
     data = {
             'command': ByteToHex(message[0]),
             'address': ByteToHex(message[1]),
             'parameter': ByteToHex(message[2]),
             'checksum': ByteToHex(message[3]),
-            
-    }    
-    print data
+
+    }
+
+    return data
+
+print send(hexstr, serObj)
