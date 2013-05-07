90706b97eb93b2dfd419c743aab42f51d53a83e5
kuba <kuba@kucharski.it>
2013-05-08 00:39:00 +0200

    Checksum check | fixed

--- metro.py
+++ metro.py
@@ -51,7 +51,7 @@ def sendBytes(byteStr, serObj):
     }
 
     #checksum check
-    print "Checksum check: " + countCheckSum(message[0],message[1],message[2])
+    print "Checksum check: " + str(countCheckSum(message[0],message[1],message[2]))
 
     return data
 
