045f7399b2cd63cb39a16ea7358a7c0a415b3176
kuba <kuba@kucharski.it>
2013-05-09 21:30:28 +0200

    fix of the construct request

--- hexbyte.py
+++ hexbyte.py
@@ -12,15 +12,6 @@ def ByteToHex( byteStr ):
     """
     Convert a byte string to it's hex string representation e.g. for output.
     """
-    
-    # Uses list comprehension which is a fractionally faster implementation than
-    # the alternative, more readable, implementation below
-    #   
-    #    hex = []
-    #    for aChar in byteStr:
-    #        hex.append( "%02X " % ord( aChar ) )
-    #
-    #    return ''.join( hex ).strip()        
 
     return ''.join( [ "%02X " % ord( x ) for x in byteStr ] ).strip()
 
@@ -31,11 +22,7 @@ def HexToByte( hexStr ):
     Convert a string hex byte values into a byte string. The Hex Byte values may
     or may not be space separated.
     """
-    # The list comprehension implementation is fractionally slower in this case    
-    #
-    #    hexStr = ''.join( hexStr.split(" ") )
-    #    return ''.join( ["%c" % chr( int ( hexStr[i:i+2],16 ) ) \
-    #                                   for i in range(0, len( hexStr ), 2) ] )
+
  
     bytes = []
 
