22886a26ae0cb80b8bf419d090a967aa0027605b
kuba <kuba@kucharski.it>
2013-05-09 19:44:47 +0200

    fix of the construct request

--- test.py
+++ test.py
@@ -36,6 +36,15 @@ def makeCommand(command,setgroup,address,parameter):
     bits = pack('uint:5, uint:1, uint:10, uint:8, uint:8',
                 command,setgroup,address,parameter,34)
 
+    byte1,byte2,byte3,byte4  = bits.unpack('bytes:1,bytes:1,bytes:1,bytes:1')
+    listOfBytes = [byte1,byte2,byte3]
+
+    checksum = sum(map(ord, listOfBytes))
+    if checksum>128: checksum = checksum - 128
+
+    bits = pack('uint:5, uint:1, uint:10, uint:8, uint:8',
+                command,setgroup,address,parameter,checksum)
+
     return ByteToHex(bits.bytes)
 
 print makeCommand(2,0,9,9)
