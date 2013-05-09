85aa63a271deec953e7f6fdcdb7d78503c89679d
kuba <kuba@kucharski.it>
2013-05-09 20:41:27 +0200

    fix of the construct request

--- test.py
+++ test.py
@@ -12,25 +12,7 @@ serObj = serial.Serial('/dev/ttyUSB1',
                        )
 
 
-hexstr = '10 09 09 22'
-
-#hexstr2 = constructRequest('GetEE',1023,1)
-
-print "Constr"
-#print ByteToHex(hexstr2)
-print hexstr
-
-print sendHex(hexstr, serObj)
-
-#print sendBytes(hexstr2,serObj)
-
-
 from bitstring import Bits, pack
-testBits = Bits(bytes=HexToByte(hexstr))
-first,second,third,fourth,fifth = testBits.unpack('uint:5, uint:1, uint:10, uint:8, uint:8')
-
-print first,second,third,fourth,fifth
-
 
 def makeCommand(command,setgroup,address,parameter):
     bits = pack('uint:5, uint:1, uint:10, uint:8, uint:8',
@@ -47,4 +29,7 @@ def makeCommand(command,setgroup,address,parameter):
 
     return ByteToHex(bits.bytes)
 
-print makeCommand(2,0,9,9)
+hexstr = makeCommand(2,0,9,9)
+
+print hexstr
+print sendHex(hexstr, serObj)
