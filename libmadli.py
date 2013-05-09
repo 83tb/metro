7763ef75212552f14e1cc3bf36ddd969b8cfedb7
kuba <kuba@kucharski.it>
2013-05-09 21:04:03 +0200

    fix of the construct request

--- libmadli.py
+++ libmadli.py
@@ -18,27 +18,27 @@ commands = {
 
 
 st7st4 = {
-    0, 'Ok',
-    1, 'PFC High',
-    2, 'PFC Low',
-    3, 'Cap mode',
-    4, 'Switch Off',
-    5, 'Overtemperature',
-    6, 'Extinguish',
-    7, 'Vin low',
-    8, 'Error'
+    0: 'Ok',
+    1: 'PFC High',
+    2: 'PFC Low',
+    3: 'Cap mode',
+    4: 'Switch Off',
+    5: 'Overtemperature',
+    6: 'Extinguish',
+    7: 'Vin low',
+    8: 'Error'
 }
 
 
 st3st0 = {
-    0, 'Off',
-    1, 'Not ignite',
-    2, 'Start',
-    3, 'On',
-    4, 'Cap test',
-    5, 'Ignition',
-    6, 'Warm-up',
-    7, 'Normal',
+    0: 'Off',
+    1: 'Not ignite',
+    2: 'Start',
+    3: 'On',
+    4: 'Cap test',
+    5: 'Ignition',
+    6: 'Warm-up',
+    7: 'Normal',
 
 }
 
