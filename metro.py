1c1d0a22b0b61a30e33fbcbd580e2fe8effb9d27
kuba <kuba@kucharski.it>
2013-06-19 00:06:57 +0200

    deploy.sh

--- metro.py
+++ metro.py
@@ -44,10 +44,10 @@ def sendBytes(byteStr, serObj):
     """
 
     serObj.write(byteStr)
-    byte = serObj.read()
+    #byte = serObj.read()
 
-    if byte:
-        message = byte + readbytes(4,serObj)
+    #if byte:
+    message = readbytes(4,serObj)
 
     data = {
 
